37457373493b66fb9198ce0d6e78f219774c2bce
valery-shvetsov <113056546+valery-shvetsov@users.noreply.github.com>
2022-09-16 23:23:39 +0300

    Ex2

---
+++ Ex2/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.WriteLine("Введите число 1");
+string AStr = Console.ReadLine();
+int A = int.Parse(AStr);
+Console.WriteLine("Введите число 2");
+string BStr = Console.ReadLine();
+int B = int.Parse(BStr);
+Console.WriteLine("Введите число 3");
+string CStr = Console.ReadLine();
+int C = int.Parse(CStr);
+int max=A;
+if(A<B) 
+{
+    max=B;
+}
+if (A<C)
+{
+    max=C;
+}
+Console.Write("Максимальное число= " );
+Console.Write(max);
