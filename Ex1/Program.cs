711be8ce6196242b8f205007ea47d3e4ba0459d6
valery-shvetsov <113056546+valery-shvetsov@users.noreply.github.com>
2022-09-16 22:55:05 +0300

    Ex1

---
+++ Ex1/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите число 1");
+string AStr = Console.ReadLine();
+int A = int.Parse(AStr);
+Console.WriteLine("Введите число 2");
+string BStr = Console.ReadLine();
+int B = int.Parse(BStr);
+
+if (A>B) Console.WriteLine("число 1 больше, чем число 2");
+else Console.WriteLine("число 2 больше, чем число 1");
+Console.WriteLine(A);
+Console.WriteLine(B);
