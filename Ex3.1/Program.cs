b6868d3a8d7f03268c918e4ea59b24482c978400
valery-shvetsov <113056546+valery-shvetsov@users.noreply.github.com>
2022-09-18 19:11:26 +0300

    Ex3.1

---
+++ Ex3.1/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Введите целое число более 2 ");
+string AStr = Console.ReadLine();
+int A = int.Parse(AStr);
+int I=2;
+Console.Write("Четные числа менее ");
+Console.WriteLine(A);
+
+while (I<A)
+{
+ Console.WriteLine(I); 
+ I=I+2;
+}
+Console.WriteLine("Задача выполнена");
