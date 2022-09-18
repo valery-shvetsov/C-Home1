119e2811c2649f431aa7953bc13c69e792f37e1b
valery-shvetsov <113056546+valery-shvetsov@users.noreply.github.com>
2022-09-18 18:37:52 +0300

    Ex3

---
+++ Ex3/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Введите число 1");
+string AStr = Console.ReadLine();
+double A = double.Parse(AStr);
+if ( A%2==0 )
+{
+    Console.WriteLine("Число четное");
+}
+else
+Console.WriteLine("Число нечетное");
